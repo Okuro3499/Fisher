f98e255808f1a7fd303be01b4b33eae7061cbfd6
Gideon Okuro <gideonollonde@gmail.com>
2020-07-31 16:10:08 +0300

    add a success login toast

--- app/src/main/java/com/jade/fisher/MainActivity.java
+++ app/src/main/java/com/jade/fisher/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "Successful login", Toast.LENGTH_LONG).show();
             }
         });
     }
