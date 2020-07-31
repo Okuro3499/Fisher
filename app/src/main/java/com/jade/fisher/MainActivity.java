67b9cd313337f09e40b5e92684039df63ed794c7
Gideon Okuro <gideonollonde@gmail.com>
2020-07-31 11:31:50 +0300

    create an onclick listener and add a toast

--- app/src/main/java/com/jade/fisher/MainActivity.java
+++ app/src/main/java/com/jade/fisher/MainActivity.java
@@ -3,7 +3,9 @@ package com.jade.fisher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mViewProductsButton;
@@ -13,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mViewProductsButton = (Button)findViewById(R.id.viewProductsButton);
+        mViewProductsButton = (Button) findViewById(R.id.viewProductsButton);
+        mViewProductsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "It Works", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
