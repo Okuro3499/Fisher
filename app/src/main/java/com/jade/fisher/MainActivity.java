a64a3d451c9a5a55b017b6994c85cd03f9704b26
Gideon Okuro <gideonollonde@gmail.com>
2020-07-31 11:50:07 +0300

    create ProductsActivity and add intent for navigation

--- app/src/main/java/com/jade/fisher/MainActivity.java
+++ app/src/main/java/com/jade/fisher/MainActivity.java
@@ -2,6 +2,7 @@ package com.jade.fisher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mViewProductsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "It Works", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
+                startActivity(intent);
             }
         });
     }
