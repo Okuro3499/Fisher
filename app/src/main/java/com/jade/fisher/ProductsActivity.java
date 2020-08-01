57b8490921b6c38e6f4c76c569acee30e223c70e
Gideon Okuro <gideonollonde@gmail.com>
2020-08-01 18:01:06 +0300

    add List View and an array adapter and implement them

--- app/src/main/java/com/jade/fisher/ProductsActivity.java
+++ app/src/main/java/com/jade/fisher/ProductsActivity.java
@@ -6,27 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProductsActivity extends AppCompatActivity {
-    public static final String TAG = ProductsActivity.class.getSimpleName();
     private Button mSearchButton;
     private EditText mSearchEditText;
+    private ListView mListView;
     private TextView mSearchTextView;
-    private String[] fishes = new String[] {"Guppy", "Neon Tetra", "Zebrafish", "Tiger barb", "Green swordtail", "Clown loach", "Red loinfish", "Bala shark",
+    private String[] fishes = new String[] {"Guppy", "Neon Tetra", "Zebra fish", "Tiger barb", "Green swordtail", "Clown loach", "Red lionfish", "Bala shark",
                                             "Pao abei", "Gold Fish"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+        mListView = (ListView) findViewById(R.id.listView);
         mSearchEditText = (EditText) findViewById(R.id.searchEditText);
-        mSearchButton = (Button) findViewById(R.id.searchButton);
 
-        mSearchButton.setOnClickListener(new View.OnClickListener(){
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, fishes);
+        mListView.setAdapter(adapter);
+
+        mSearchButton = (Button) findViewById(R.id.searchButton);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String search = mSearchEditText.getText().toString();
@@ -38,5 +45,14 @@ public class ProductsActivity extends AppCompatActivity {
                 mSearchTextView.setText("Products found: " + search);
             }
         });
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String fish = ((TextView)view).getText().toString();
+            Toast.makeText(ProductsActivity.this, fish, Toast.LENGTH_LONG).show();
+        }
+    });
     }
+
 }
