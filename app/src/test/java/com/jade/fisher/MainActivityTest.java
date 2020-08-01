1a9a090d37462fe62e3fb5445fd64964f0f143ea
Gideon Okuro <gideonollonde@gmail.com>
2020-08-01 11:36:07 +0300

    create MainActivityTest class

---
+++ app/src/test/java/com/jade/fisher/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.jade.fisher;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+
+}
