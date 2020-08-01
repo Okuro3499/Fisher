aff9e68efb8d3cd2da0526617592c4e5b4e3263b
Gideon Okuro <gideonollonde@gmail.com>
2020-08-01 16:41:19 +0300

    change app name font

--- app/src/androidTest/java/com/jade/fisher/ProductsActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/jade/fisher/ProductsActivityInstrumentationTest.java
@@ -5,7 +5,16 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -13,4 +22,24 @@ public class ProductsActivityInstrumentationTest {
     @Rule
     public ActivityTestRule<ProductsActivity> activityTestRule =
             new ActivityTestRule<>(ProductsActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.searchEditText)).perform(typeText("Coy Fish"))
+                .check(matches(withText("Coy Fish")));
+    }
+
+    @Test
+    public void searchIsPassedToProductsActivity() {
+        String search = "Coy Fish";
+        onView(withId(R.id.searchEditText)).perform(typeText(search)).perform(closeSoftKeyboard());
+        try {
+            Thread.sleep(250);
+        } catch (InterruptedException e) {
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.searchButton)).perform(click());
+        onView(withId(R.id.searchTextView)).check(matches
+                (withText("Products found: " + search)));
+    }
 }
