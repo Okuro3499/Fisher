6998e5c8c23ea4d4307ae5150c6f1777fb218cc7
Gideon Okuro <gideonollonde@gmail.com>
2020-08-01 11:27:59 +0300

    passed data with intent extras

--- app/src/main/java/com/jade/fisher/ProductsActivity.java
+++ app/src/main/java/com/jade/fisher/ProductsActivity.java
@@ -2,17 +2,20 @@ package com.jade.fisher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProductsActivity extends AppCompatActivity {
     public static final String TAG = ProductsActivity.class.getSimpleName();
     private Button mSearchButton;
     private EditText mSearchEditText;
+    private TextView mSearchTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +23,18 @@ public class ProductsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_products);
         mSearchEditText = (EditText) findViewById(R.id.searchEditText);
         mSearchButton = (Button) findViewById(R.id.searchButton);
-        mSearchButton.setOnClickListener(new View.OnClickListener(){
 
+        mSearchButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 String search = mSearchEditText.getText().toString();
                 Log.d(TAG, search);
                 Toast.makeText(ProductsActivity.this, search, Toast.LENGTH_LONG).show();
+
+                mSearchTextView = (TextView) findViewById(R.id.searchTextView);
+                Intent intent = getIntent();
+                String psearch = intent.getStringExtra("psearch");
+                mSearchTextView.setText("Products found: " + psearch);
             }
         });
     }
