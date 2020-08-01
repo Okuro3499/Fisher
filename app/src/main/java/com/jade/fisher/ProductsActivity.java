0756745894c0e943f719755b3e6bda9d4f868c2a
Gideon Okuro <gideonollonde@gmail.com>
2020-08-01 10:56:42 +0300

    gather data from editText and show in logcat

--- app/src/main/java/com/jade/fisher/ProductsActivity.java
+++ app/src/main/java/com/jade/fisher/ProductsActivity.java
@@ -3,12 +3,31 @@ package com.jade.fisher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProductsActivity extends AppCompatActivity {
+    public static final String TAG = ProductsActivity.class.getSimpleName();
+    private Button mSearchButton;
+    private EditText mSearchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+        mSearchEditText = (EditText) findViewById(R.id.searchEditText);
+        mSearchButton = (Button) findViewById(R.id.searchButton);
+        mSearchButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String search = mSearchEditText.getText().toString();
+                Log.d(TAG, search);
+                Toast.makeText(ProductsActivity.this, search, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
