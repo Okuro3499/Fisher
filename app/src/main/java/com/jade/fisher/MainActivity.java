9dbc32ef5e38aebe29b5b621fb2673baec7dfd6d
Gideon Okuro <gideonollonde@gmail.com>
2020-08-09 14:40:32 +0300

    add form validation

--- app/src/main/java/com/jade/fisher/MainActivity.java
+++ app/src/main/java/com/jade/fisher/MainActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextEmail;
+
     @BindView(R.id.viewProductsButton)Button mViewProductsButton;
     @BindView(R.id.loginButton) Button mLoginButton;
     @BindView(R.id.signUpButton) Button mSignUpButton;
+
+    private AwesomeValidation awesomeValidation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +32,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+
         mViewProductsButton.setOnClickListener(this);
         mLoginButton.setOnClickListener(this);
         mSignUpButton.setOnClickListener (this);
+
+        awesomeValidation.addValidation(this, R.id.editTextEmail, Patterns.EMAIL_ADDRESS, R.string.emailerror);
     }
 
     @Override
@@ -34,9 +52,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
         if (v == mLoginButton) {
-            Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
-            startActivity(intent);
-            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+            if (awesomeValidation.validate()){
+                Intent intent = new Intent(MainActivity.this, ProductsActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+            }
         }
         if(v == mSignUpButton) {
             Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
--- app/src/main/java/com/jade/fisher/SignUpActivity.java
+++ app/src/main/java/com/jade/fisher/SignUpActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextFirstName, editTextLastName, editTextNewEmail;
+
     @BindView(R.id.createAccountButton) Button mCreateAccountButton;
+
+    private AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +29,30 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        editTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
+        editTextLastName = (EditText) findViewById(R.id.editTextLastName);
+        editTextNewEmail = (EditText) findViewById(R.id.editTextNewEmail);
+
+        mCreateAccountButton = (Button) findViewById(R.id.createAccountButton);
+
         mCreateAccountButton.setOnClickListener(this);
+
+        awesomeValidation.addValidation(this, R.id.editTextFirstName, "^[A-Za-z\\\\s]{1,}[\\\\.]{0,1}[A-Za-z\\\\s]{0,}$", R.string.nameerror);
+        awesomeValidation.addValidation(this, R.id.editTextLastName, "^[A-Za-z\\\\s]{1,}[\\\\.]{0,1}[A-Za-z\\\\s]{0,}$", R.string.nameerror);
+        awesomeValidation.addValidation(this, R.id.editTextNewEmail, Patterns.EMAIL_ADDRESS, R.string.emailerror);
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == mCreateAccountButton) {
-            Intent intent = new Intent(SignUpActivity.this, ProductsActivity.class);
-            startActivity(intent);
-            Toast.makeText(SignUpActivity.this, "Account created successfully", Toast.LENGTH_LONG).show();
+            if (awesomeValidation.validate()){
+                Intent intent = new Intent(SignUpActivity.this, ProductsActivity.class);
+                startActivity(intent);
+                Toast.makeText(SignUpActivity.this, "Account created successfully", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
