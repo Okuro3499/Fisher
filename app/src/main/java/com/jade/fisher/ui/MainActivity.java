ae3daf200b2b7a41a43f870a1994bfccb21d9afe
Gideon Okuro <gideonollonde@gmail.com>
2020-08-20 09:20:46 +0300

    add a login with password method

--- app/src/main/java/com/jade/fisher/ui/MainActivity.java
+++ app/src/main/java/com/jade/fisher/ui/MainActivity.java
@@ -1,15 +1,21 @@
 package com.jade.fisher.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.jade.fisher.R;
 
@@ -53,6 +59,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(view == mPasswordLoginButton) {
             loginWithPassword();
         }
+    }
+
+    private void loginWithPassword() {
+        String email = mEditTextEmail.getText().toString().trim();
+        String password = mEditTextPassword.getText().toString().trim();
+        if (email.equals("")) {
+            mEditTextEmail.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mEditTextPassword.setError("Password cannot be blank");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
 
